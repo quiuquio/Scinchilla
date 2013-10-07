074290c8d5c72c1e886be5111d01f47714c178ed
quiuquio <betoparcus@gmail.com>
2013-10-07 21:04:26 +0200

    Added graph, phenotype names, etc, etc

--- calculator.js
+++ calculator.js
@@ -6,19 +6,85 @@
 	this.dominantAllele = dominantAllele;// 0 for none aa, 1 for allele1 Aa, 2 for allele2 aA, 3 for both AA;
 }*/
 
-function genotypePresence(){
-	name:"";
-	quant:0;
-}
+/*
+Modifica una stringa all'indice index
+Example: 
+	var hello="Hello World";
+	alert(hello.replaceAt(3, "a"));
+*/
+String.prototype.replaceAt=function(index, character) {
+      return this.substr(0, index) + character + this.substr(index+character.length);
+   }
 
-function trait(identifier){
+
+/*function genoTyper(genoma, traitName){
+	if(traitName === "Non Beige"){
+		genoma.replaceAt(0,"a");
+		genoma.replaceAt(1,"a");
+	}
+	else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}else if(traitName === ""){
+		genoma.replaceAt(,"");
+		genoma.replaceAt(,"");
+	}
+}*/
+
+
+function trait(identifier, genoma){
 	switch (identifier){
 		case "p1T1no":
 		case "p2T1no":
 			this.traitName = "Non Beige";
-			this.dominance = 0;
+			this.dominance = 0; 
 			this.allele1 = "a";
-			this.allele2 = "a"; 
+			this.allele2 = "a";
 			this.dominantAllele = 0;
 			break;
 		case "p1T1Etero":
@@ -155,16 +221,16 @@ function trait(identifier){
 			this.allele2 = "g"; 
 			this.dominantAllele = 0;
 			break;
-		case "p1T7Etero":
-		case "p2T7Etero":
+		case "p1T71":
+		case "p2T71":
 			this.traitName = "Ebony 1";
 			this.dominance = 1;
 			this.allele1 = "G";
 			this.allele2 = "g"; 
 			this.dominantAllele = 1;
 			break;
-		case "p1T7Homo":
-		case "p2T7Homo":
+		case "p1T72":
+		case "p2T72":
 			this.traitName = "Ebony 2";
 			this.dominance = 1;
 			this.allele1 = "G";
@@ -238,6 +304,7 @@ function parent(traits){
 	this.T8 = traits[7];
 }
 
+/*Genera una stringa di tipo aabbccddee...*/
 function getCombinations(chars) {
   var result = [];
   var f = function(prefix, chars) {
@@ -303,8 +370,13 @@ function checkForDuplicates(arr){
 	return false;
 }
 
+/**
+@function {String} 
+@param {parent} a parent Object();
+@returns String di tipo "aabbccddee"...etc
+*/
 function generateCrossArray(parent1){
-	crossArray = [];
+	var crossArray = [];
 	for (key in parent1){
 		var trait = parent1[key];
 		for(prop in trait){
@@ -340,8 +412,16 @@ function generateResults(arr1, arr2){
 		for(var j = 0; j < size; j++){
 			resArray[i+(j*size)] = "";
 			for(var k = 0; k < size; k++){
-				resArray[i+(j*size)] += arr1[i].charAt(k);
-				resArray[i+(j*size)] += arr2[j].charAt(k);
+				gene1 =arr1[i].charAt(k);
+				gene2 = arr2[j].charAt(k);
+				//fa in modo che le maiuscole siano sempre prima delle minuscole.
+				if(arr2[j].charAt(k) == arr2[j].charAt(k).toUpperCase()){
+					resArray[i+(j*size)] += arr2[j].charAt(k);
+					resArray[i+(j*size)] += arr1[i].charAt(k);
+				}else{
+					resArray[i+(j*size)] += arr1[i].charAt(k);
+					resArray[i+(j*size)] += arr2[j].charAt(k);
+				}
 			}
 		}
 	}
@@ -350,8 +430,11 @@ function generateResults(arr1, arr2){
 
 function getPercentage(values){
 	genoTypes = [];
+	var tmp;
 	//console.log(genoTypes);
 	for(var i = 0; i<values.length; i++){
+		//tmp = values[i].split("").sort();
+		//values[i] = tmp.join("");
 		if (genoTypes[values[i]]>= 0){
 			genoTypes[values[i]]++;
 		}else{
@@ -365,47 +448,223 @@ function getPercentage(values){
 	return genoTypes;
 }
 
-function phenotyper(genoTypes){
+function phenotyper(genoma){
 
 }
 
-
+var names;
 $(document).ready(function() {
+	jQuery.get('fenotipi.csv', function(data) {
+			names = $.csv.toArrays(data);
+    		console.log(names);
+		});
 	$("#calculate").click( function(){
-		var tratti = [];
-		$("#parent1 :radio:checked").each(
-			function(index) {
-				var input = $(this).prop("value","on");
-				tratti[index] = new trait(input.prop("id"));
-			});
-		tratti.push(new trait("p1T8Homo"));
-		parent1 = new parent(tratti);
-		var tratti = [];
-		$("#parent2 :radio:checked").each(
-			function(index) {
-				var input = $(this).prop("value","on");
-				tratti[index] = new trait(input.prop("id"));
-			});
-		tratti.push(new trait("p1T8Homo"));
-		parent2 = new parent(tratti);
-		var crossArray1 = generateCrossArray(parent1);
-		var crossArray2 = generateCrossArray(parent2);
-		console.log("You are crossing: " + crossArray1 + " x " + crossArray2);
-		
-		finalArray1 = generateFinalArray(crossArray1);
-		finalArray2 = generateFinalArray(crossArray2);
-		//console.log(finalArray1);
-		//console.log(finalArray2);
-
-		var res = generateResults(finalArray1, finalArray2);
-		var percentages = getPercentage(res);
-		console.log(percentages);
-		percentages.length;
-		//console.log(parent1);
-		//console.log(parent2);
-
-		/*Object.keys(tratti).forEach(function(key) {
-		    console.log(key, tratti[key]);
-		});*/
+		$('#calculate').hide();
+		$('#wait').show();
+		setTimeout(main,500);
+		//$('#wait').hide();
+		$('#calculate').show();
 	});
 });
+
+function translate(percentage){
+	var tmp = [];
+	var res = []
+	for (key in percentage){
+		trans = find(key,names);
+		tmp[key] = trans[key];
+		//console.log(tmp);
+		res.push({phenoType:tmp[key],percentage: percentage[key]});
+	}
+	return res;
+	//console.log(res);
+}
+
+function find(thing, theArray) {
+    var results, col, row, subArray;
+
+    results = []; // Empty array
+    results[thing] ="";
+    for (row = 0; row < theArray.length; ++row) {
+        subArray = theArray[row];
+        for (col = 0; col < subArray.length; ++col) {
+            value = subArray[col];
+            if (value == thing) { // or whatever your criterion
+            	results[thing] += subArray[col-1];
+                //results.push({thing: thing, row: row, col: col});
+            }
+        }
+    }
+    //console.log(results[thing]);
+    if(results[thing] == ""){
+    	results[thing] = getGenesFormula(thing);
+    	//console.log("Igot there!");
+    }
+
+    return results;
+}
+
+function getGenesFormula(genes){
+	var res = "";
+	if(genes.indexOf("Aa")>=0){
+		res += "Beige Hetero + ";
+	}
+	if(genes.indexOf("AA")>=0){
+		res += "Beige Homo + ";
+	}
+	if(genes.indexOf("Bb")>=0){
+		res += "Violet Carrier + ";
+	}
+	if(genes.indexOf("BB")>=0){
+		res += "Violet + ";
+	}
+	if(genes.indexOf("Cc")>=0){
+		res += "Saphire Carrier + ";
+	}
+	if(genes.indexOf("CC")>=0){
+		res += "Saphire + ";
+	}
+	if(genes.indexOf("Dd")>=0){
+		res += "German Violet Carrier + ";
+	}
+	if(genes.indexOf("DD")>=0){
+		res += "German Violet + ";
+	}
+	if(genes.indexOf("Ee")>=0){
+		res += "White + ";
+	}
+	if(genes.indexOf("EE")>=0){
+		res += "LETAL FACTOR FOR WHITE + ";
+	}
+	if(genes.indexOf("Ff")>=0){
+		res += "TOV + ";
+	}
+	if(genes.indexOf("FF")>=0){
+		res += "LETAL FACTOR FOR TOV + ";
+	}
+	if(genes.indexOf("Gg")>=0){
+		res += "Ebony Carrier + ";
+	}
+	if(genes.indexOf("GG")>=0){
+		res += "Ebony + ";
+	}
+	if(res!=""){
+		res = res.slice(0,-3);
+	}
+	return res;
+}
+
+
+function main(){
+	/*http://stackoverflow.com/questions/750358/with-jquery-how-can-i-implement-a-page-loading-animation*/
+	var tratti = [];
+	$("#parent1 :radio:checked").each(
+		function(index) {
+			var input = $(this).prop("value","on");
+			tratti[index] = new trait(input.prop("id"));
+		});
+	tratti.push(new trait("p1T8Homo"));
+	parent1 = new parent(tratti);
+	var tratti = [];
+	$("#parent2 :radio:checked").each(
+		function(index) {
+			var input = $(this).prop("value","on");
+			tratti[index] = new trait(input.prop("id"));
+		});
+	tratti.push(new trait("p1T8Homo"));
+	parent2 = new parent(tratti);
+	var crossArray1 = generateCrossArray(parent1);
+	var crossArray2 = generateCrossArray(parent2);
+	console.log("You are crossing: " + crossArray1 + " x " + crossArray2);
+
+	var finalArray1 = generateFinalArray(crossArray1);
+	var finalArray2 = generateFinalArray(crossArray2);
+	//console.log(finalArray1);
+	//console.log(finalArray2);
+
+	var res = generateResults(finalArray1, finalArray2);
+	var percentages = getPercentage(res);
+	//console.log(percentages);
+	percentages = translate(percentages);
+	console.log(percentages);
+	var percArray = []
+	for(var prop in percentages){
+		console.log(JSON.stringify(percentages[prop],null,4));
+		percArray[prop] =[ percentages[prop].phenoType, percentages[prop].percentage]; 
+	}
+	console.log(percArray);
+	//console.log(JSON.stringify(percentages,null,4));
+	//$( "#results" ).html( JSON.stringify(percentages,null,4) );
+	$( "#results" ).html( '<div class="container" style="min-width: 310px; width: 75%; margin: 0 auto"></div>');    	
+		
+	// Build the chart
+	$('.container').empty();
+    $('.container').highcharts({
+        chart: {
+        	renderTo: 'container',
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Risultato incrocio'
+        },
+        tooltip: {
+    	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    formatter: function() {
+                        return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Risultato',
+            data: percArray /*[['phenotype' , percentage], ['phenotype' , percentage], ...] */
+            /*data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]*/
+        }],
+        colors: Highcharts.map(Highcharts.getOptions().colors, function(color) {
+		    return {
+		        radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+		        stops: [
+		            [0, color],
+		            [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+		        ]
+		    };
+		})
+    });
+
+
+
+
+
+
+	//console.log(parent1);
+	//console.log(parent2);
+
+	/*Object.keys(tratti).forEach(function(key) {
+	    console.log(key, tratti[key]);
+	});*/
+
+}
